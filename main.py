8c3267fc46ea1dff0adb54252a487a419e2e0175
Tovics <tovics1@gmail.com>
2017-07-03 00:53:08 +0200

    heroku

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, redirect, url_for, jsonify
-app = Flask(__name__)
+import os
+import psycopg2
+import urllib
 
+urllib.parse.uses_netloc.append('postgres')
+url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+connection = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+app = Flask(__name__)
 
 @app.route('/_get_current_user')
 def get_current_user():
