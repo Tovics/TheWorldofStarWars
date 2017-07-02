69297a1319fd9f88f529f483d7021444f6a88440
Tovics <tovics1@gmail.com>
2017-07-02 23:44:32 +0200

    deployable

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, url_for, jsonify
+app = Flask(__name__)
+
+
+@app.route('/_get_current_user')
+def get_current_user():
+    return jsonify(username=g.user.username,
+                   email=g.user.email,
+                   id=g.user.id)
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('planets'))
+
+
+@app.route('/world_of_star_wars')
+def planets():
+    return render_template('index.html')
+
+
+def main():
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    main()
---
+++ static/script.js
@@ -0,0 +1,105 @@
+
+$(document).ready(function() {
+
+    loadPlanets();
+    defineNavigationButtonsListeners();
+});
+
+function defineNavigationButtonsListeners() {
+    $('#controll-buttons .btn').click(function() {
+        var link = $(this).data('link');
+        loadPlanets(link);
+    });
+}
+
+function defineResidentsButtonsListeners() {
+    $('#planet_data .resident').click(function() {
+        $('#residents-modal').modal('show');
+    });
+}
+
+function loadPlanets(url = 'https://swapi.co/api/planets') {
+    
+    $.ajax({
+        dataType: "json",
+        url: url,
+        success: function(response) {
+            var residentTable = $("#residents_modulo")
+            var planets = response.results;
+            var planetTable = $("#planet_data");
+            planetTable.find("tr:not(:first)").remove();
+
+
+            for (var i = 0; i < planets.length; i++) {
+                var planetData = planets[i];
+                var diaInt = parseInt(planetData.diameter);
+                var diameter = diaInt.toLocaleString();
+            
+
+                population = 'unknown';
+                if (planetData.population !== 'unknown') {
+                    var populationInt = parseInt(planetData.population);
+                    population = populationInt.toLocaleString() + ' people';
+                }
+                surfaceWater = 'unknown';
+                if (planetData.surface_water !== 'unknown') {
+                    var surfaceWater = parseInt(planetData.surface_water);
+                    surfaceWater = surfaceWater.toLocaleString() + '%';
+                }
+                planetResidents = 'NoKnownResidents';
+                if (planetData.residents.length !== 0) {
+                    var planetResidentsString = planetData.residents.length;
+                    planetResidents = planetResidentsString + 'resident(s)';
+                }
+                            
+                var planetRow = `
+                <tr>
+                    <td>${planetData.name}</td>
+                    <td class="diam">${diameter}</td>
+                    <td>${planetData.climate}</td>
+                    <td>${planetData.terrain}</td>
+                    <td class="water">${surfaceWater}</td>
+                    <td class="ppl">${population}</td>
+                    <td><input class="btn btn-primary resident" type="button" value=${planetResidents}></td>
+                </tr>`;
+
+
+                var residentRow = `
+                <tr>              
+                    <td>${planetData.residents.name}</td>
+                    <td>${planetData.residents.height}</td>
+                    <td>${planetData.residents.mass}</td>
+                    <td>${planetData.residents.hair_color}</td>
+                    <td>${planetData.residents.skin_color}</td>
+                    <td>${planetData.residents.eye_color}</td>
+                    <td>${planetData.residents.birth_year}</td>
+                    <td>${planetData.residents.gender}</td>
+                </tr>`;
+
+                residentTable.append(residentRow);
+                planetTable.append(planetRow);
+                defineResidentsButtonsListeners();
+            }
+
+            setNavigationButtonLinks(response.next, response.previous);
+        }
+    });
+}
+
+function setNavigationButtonLinks(nextLink, prevLink) {
+    var nextButton = $('#next');
+    var prevButton = $('#previous');
+
+    if (nextLink !== null) {
+        nextButton.data('link', nextLink);
+        nextButton.show();
+    } else {
+        nextButton.hide();
+    }
+    if (prevLink !== null) {
+        prevButton.data('link', prevLink);
+        prevButton.show();
+    } else {
+        prevButton.hide();
+    }
+}
